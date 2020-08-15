991368ac9d89f1fabfba0a1fa1bdb9379645fc92
Artur Konopka <artur.konopka.1994@gmail.com>
2020-08-15 15:49:08 +0100

    routes editted

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :matches, only: [:index, :show, :new, :create] do
+  resources :matches do
     resources :bookings, only: [:new, :create]
   end
-  resources :venues, only: [:create, :index, :show] do
+  resources :venues, only: [:create, :index, :show, :edit] do
     resources :reviews, only: [:create]
   end
   resources :chatroom, only: [:index, :show] do
     resources :messages, only: [:create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :bookings, only: [:destroy, :show]
   get "/dashboard", to: "pages#dashboard"
 end
