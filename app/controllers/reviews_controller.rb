0ac30fc4234214549d9ca538342f3452f966891b
matt evans <mattevo10@gmail.com>
2020-08-15 16:47:06 +0100

    reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+
+  # def new
+  #   @venue = Venue.find(params[:venue_id])
+  #   @review = Review.new
+  # end
+
+  def create
+    @venue = Venue.find(params[:venue_id])
+    @review = Review.new(review_params)
+    @review.venue = @venue
+    @review.user = current_user
+    if @review.save
+      # redirect_to Something_path
+    else
+      render 'venue/show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
