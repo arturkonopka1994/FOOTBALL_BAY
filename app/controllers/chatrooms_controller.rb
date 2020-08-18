41555f511cd8ff3e1c4d9dff774b88c90de69773
Artur Konopka <artur.konopka.1994@gmail.com>
2020-08-18 19:40:45 +0100

    controller for chatrooms created

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -7,11 +7,5 @@ class ChatroomsController < ApplicationController
     @match = Match.find(params[:match_id])
     @chatroom = Chatroom.find(params[:id])
   end
-
-  private
-
-  def match_params
-    params.require(:match).permit(:skill_level, :no_of_players, :start_time, :end_time,
-    :match_description, :gender)
-  end
 end
+
