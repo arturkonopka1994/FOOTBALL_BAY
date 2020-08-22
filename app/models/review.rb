5f31b80b7c5831316bd9e7bf9cfb0986156649ad
matt evans <mattevo10@gmail.com>
2020-08-22 12:49:21 +0100

    finished action text. added reivews to venues

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,5 @@ class Review < ApplicationRecord
   belongs_to :venue
   has_rich_text :rich_body
 
-  validates :description, length: { minimum: 5 }, presence: true
-  validates :rating, presence: true, inclusion: { in: 0..5 }, numericality: { only_integer: true }
+  validates :rich_body, length: { minimum: 5 }, presence: true
 end
