9c65c26248efd15b1747748c2ffb78cb358f8658
Francois Potut <f.potut@gmail.com>
2020-08-25 21:01:51 +0100

    compulsory field new venue

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,5 +3,5 @@ class Venue < ApplicationRecord
 	has_many :reviews, dependent: :destroy
 	has_one_attached :photo
 	validates :name, presence: true, uniqueness: true
-	validates :city, :post_code, presence: true 
+	validates :city, :post_code, :surface, :cost_per_hour, presence: true
 end
