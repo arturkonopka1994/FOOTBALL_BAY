c4e545bfc5b1131ef120131cf447f6710834a56e
Artur Konopka <artur.konopka.1994@gmail.com>
2020-09-11 22:38:53 +0100

    Pages controller user location

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    if Rails.env.production?
-      @country = request.location.country
-      @city = request.location.city
-      @post_code = request.location.postal_code
-    end
+    
   end
 
   def dashboard
