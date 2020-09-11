0e055019f018a62f0b904d0160a486c7f61c2f03
Artur Konopka <artur.konopka.1994@gmail.com>
2020-09-11 22:39:08 +0100

    Pages controller user location added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    
+    if Rails.env.production?
+      @country = request.location.country
+      @city = request.location.city
+      @post_code = request.location.postal_code
+    end
   end
 
   def dashboard
