20a2d10528e39aa032e9e6f784a0211f0b864525
rida serroukh <rida.serroukh@gmail.com>
2020-09-15 20:58:11 +0100

    Games from past no longer on show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @matches_last = Match.last(5)
+    @matches_last = Match.select{ |match| match.start_time >= DateTime.now }
+    # @matches_last = Match.last(5)
     @matches_first = Match.first(5)
   end
 
