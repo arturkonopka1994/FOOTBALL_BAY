acfb75b45fbed2ce9afb71c13a65c4b6dbd23afb
matt evans <mattevo10@gmail.com>
2020-09-22 22:09:10 +0100

    making the matches on the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,7 @@ venue_one = Venue.create!(name: "Hire A Pitch",
     cost_per_hour: "£5",
     address_line_1: "Camden Rhino Turf, 21 Castlehaven Rd",
     surface: "Natural",
+    floodlit: true,
     price: 5)
 venue_one.photo.attach(io: File.open("app/assets/images/pitches/one.jpg"), filename: "one.jpg" )
 
@@ -30,6 +31,7 @@ venue_two = Venue.create!(name: "Power League",
     cost_per_hour: "£5",
     address_line_1: "1 Coral St, Bishops",
     surface: "Hybrid",
+    floodlit: true,
     price: 5)
 venue_two.photo.attach(io: File.open("app/assets/images/pitches/two.jpg"), filename: "two.jpg" )
 
@@ -39,6 +41,7 @@ venue_three = Venue.create!(name: "Bermondsy Pitch",
     cost_per_hour: "£5",
     address_line_1: "151 Sydney St, Chelsea",
     surface: "Artificial",
+    floodlit: false,
     price: 5)
 venue_three.photo.attach(io: File.open("app/assets/images/pitches/three.jpg"), filename: "three.jpg" )
 
@@ -48,6 +51,7 @@ venue_four = Venue.create!(name: "St Lukes",
     cost_per_hour: "£5",
     address_line_1: "Copenhagen Primary School, Treaty St, Islington",
     surface: "Hybrid",
+    floodlit: true,
     price: 5)
 venue_four.photo.attach(io: File.open("app/assets/images/pitches/four.png"), filename: "four.png" )
 
@@ -57,6 +61,7 @@ venue_five = Venue.create!(name: "Go Mammoth",
     cost_per_hour: "£5",
     address_line_1: "4-8 Vyner St",
     surface: "Natural",
+    floodlit: false,
     price: 5)
 venue_five.photo.attach(io: File.open("app/assets/images/pitches/five.png"), filename: "five.png" )
 
@@ -66,6 +71,7 @@ venue_six = Venue.create!(name: "Lofting 5 Aside",
     cost_per_hour: "£6",
     address_line_1: "Islington",
     surface: "Grass",
+    floodlit: false,
     price: 6)
 venue_six.photo.attach(io: File.open("app/assets/images/pitches/six.jpg"), filename: "six.jpg" )
 
@@ -75,6 +81,7 @@ venue_seven = Venue.create!(name: "Hireapitch UK",
     cost_per_hour: "£7",
     address_line_1: "1 Coral St, Bishop's",
     surface: "Hybrid",
+    floodlit: true,
     price: 7)
 venue_seven.photo.attach(io: File.open("app/assets/images/pitches/seven.jpg"), filename: "seven.jpg" )
 
@@ -84,6 +91,7 @@ venue_eight = Venue.create!(name: "ULTIMATE5ASIDE",
     cost_per_hour: "£5",
     address_line_1: "419 Burdett Rd",
     surface: "Synthetic",
+    floodlit: true,
     price: 5)
 venue_eight.photo.attach(io: File.open("app/assets/images/pitches/eight.jpg"), filename: "eight.jpg" )
 
@@ -93,6 +101,7 @@ venue_nine = Venue.create!(name: "Blue Hut",
     cost_per_hour: "£6",
     address_line_1: "Station, Braithwaite Street Opposite Shoreditch High Street",
     surface: "2G artificial grass",
+    floodlit: true,
     price: 6)
 venue_nine.photo.attach(io: File.open("app/assets/images/pitches/nine.jpg"), filename: "nine.jpg" )
 
@@ -102,6 +111,7 @@ venue_ten = Venue.create!(name: "Clapham Sports",
     cost_per_hour: "£7",
     address_line_1: "Black Prince Community Hub, 5 Beaufoy Walk, Prince's",
     surface: "3G artificial grass",
+    floodlit: false,
     price: 7)
 venue_ten.photo.attach(io: File.open("app/assets/images/pitches/ten.jpg"), filename: "ten.jpg" )
 
@@ -111,6 +121,7 @@ venue_eleven = Venue.create!(name: "Mile End",
     cost_per_hour: "£5",
     address_line_1: "Market Rd",
     surface: "Synthetic",
+    floodlit: true,
     price: 5)
 venue_eleven.photo.attach(io: File.open("app/assets/images/pitches/eleven.jpg"), filename: "eleven.jpg" )
 
@@ -120,6 +131,7 @@ venue_twelve = Venue.create!(name: "Lukazade Pitch",
     cost_per_hour: "£6",
     address_line_1: "Eurolink Business Centre, 49 Effra Rd, Brixton",
     surface: "Grass",
+    floodlit: false,
     price: 6)
 venue_twelve.photo.attach(io: File.open("app/assets/images/pitches/twelve.jpg"), filename: "twelve.jpg" )
 
@@ -129,6 +141,7 @@ venue_thirteen = Venue.create!(name: "Holborn pitches",
     cost_per_hour: "£5",
     address_line_1: "71-75 Shelton St, Holborn",
     surface: "Grass",
+    floodlit: false,
     price: 5)
 venue_thirteen.photo.attach(io: File.open("app/assets/images/pitches/thirteen.jpg"), filename: "thirteen.jpg" )
 
@@ -138,6 +151,7 @@ venue_fourteen = Venue.create!(name: "Powerade Football",
     cost_per_hour: "£5",
     address_line_1: "West Ealing",
     surface: "Grass",
+    floodlit: true,
     price: 5)
 venue_fourteen.photo.attach(io: File.open("app/assets/images/pitches/fourteen.JPG"), filename: "fourteen.JPG" )
 
@@ -147,6 +161,7 @@ venue_fifteen = Venue.create!(name: "5 Aside football",
     cost_per_hour: "£5",
     address_line_1: "Bethwin Adventure Playground",
     surface: "Grass",
+    floodlit: true,
     price: 5)
 venue_fifteen.photo.attach(io: File.open("app/assets/images/pitches/fifteen.jpg"), filename: "fifteen.jpg" )
 
@@ -156,6 +171,7 @@ venue_sixteen = Venue.create!(name: "The Arc",
     cost_per_hour: "£6",
     address_line_1: "Stewart's Rd",
     surface: "2G artificial grass",
+    floodlit: true,
     price: 6)
 venue_sixteen.photo.attach(io: File.open("app/assets/images/pitches/sixteen.jpg"), filename: "sixteen.jpg" )
 
@@ -165,6 +181,7 @@ venue_seventeen = Venue.create!(name: "Play Football",
     cost_per_hour: "£6",
     address_line_1: "41 S Africa Rd, Shepherd's Bush",
     surface: "Synthetic",
+    floodlit: false,
     price: 6)
 venue_seventeen.photo.attach(io: File.open("app/assets/images/pitches/seventeen.jpg"), filename: "seventeen.jpg" )
 
@@ -174,6 +191,7 @@ venue_eighteen = Venue.create!(name: "PowerLeague",
     cost_per_hour: "£6",
     address_line_1: "City Harbour NCP, Selsdon Way, Isle of Dogs",
     surface: "Hybrid",
+    floodlit: true,
     price: 6)
 venue_eighteen.photo.attach(io: File.open("app/assets/images/pitches/eighteen.jpg"), filename: "eighteen.jpg" )
 
@@ -188,26 +206,224 @@ user_two = User.create!(email: "a.morgan@gmail.com",
 puts "Users created"
 
 match_one = Match.create!(
-    title: "Cool Game 1",
+    title: "Five a side game",
     mobile_number: "+447398786653",
-    skill_level: "beginner",
-    no_of_players: 5,
-    description: 'I want to have a lovely game of football',
+    skill_level: "Just for fun",
+    no_of_players: "10 Players",
+    description: 'Its a non competative game. We are playing in either black or whites. Im bringing a ball with me so dont worry :)',
     start_time: "2pm",
-    gender: "male",
-    end_time: "3.30pm",
-    venue_id: venue_five.id,
+    end_time: "3pm",
+    venue_id: venue_one.id,
     user_id: user_one.id)
 
-match_two = Match.create!(skill_level: "advanced",
-    title: "Cool Game 2",
-    mobile_number: "+447398786653",
-    no_of_players: 7,
-    description: 'I want to have a lovely game of football',
-    start_time: "7pm",
-    gender: "female",
-    end_time: "8pm",
-    venue_id: venue_five.id,
-    user_id: user_one.id)
+# match_two = Match.create!(
+#     title: "A fun game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Just for fun",
+#     no_of_players: "14 Players",
+#     description: 'Ive just moved to the area and want to make some friends. Please come along it will be fun!',
+#     # start_time: "7pm",
+#     # end_time: "8pm",
+#     venue_id: venue_one.id,
+#     user_id: user_one.id)
+
+# match_three = Match.create!(
+#     title: "Competative Game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_four = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+    
+# match_five = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_six = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_seven = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_eight = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_nine = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_ten = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_eleven = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_twelve = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_thirteen = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_fourteen = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_fifteen = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_sixteen = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_seventeen = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_eighteen = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_nineteen = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+
+# match_twenty = Match.create!(
+#     title: "Five a side game",
+#     mobile_number: "+447398786653",
+#     skill_level: "Try Hard",
+#     no_of_players: "10 Players",
+#     description: 'I want a good competative game of football. Please only join if you are experienced',
+#     # start_time: "2pm",
+#     # end_time: "3pm",
+#     venue_id: venue_eight.id,
+#     user_id: user_one.id)
+        
 
 puts "Matches created"
